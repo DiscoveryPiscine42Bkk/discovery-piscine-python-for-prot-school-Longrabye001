07995b4e4d71536f77ab05cf76484a1235f96188
Ratanon <41043@prot.ac.th>
2025-03-11 10:14:42 +0700

    Update mult.py

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,16 +1,11 @@
-#!/usr/bin/env python3
 
-# Prompt the user for two numbers
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
-# Calculate the multiplication result
 result = num1 * num2
 
-# Display the multiplication result
 print(f"{num1} x {num2} = {result}")
 
-# Determine the sign of the result
 if result > 0:
     print("The result is positive.")
 elif result < 0:
